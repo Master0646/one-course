a5f86b8bbc4626d3c4f86bb901a21d1ac30e14cb
RewindL <928849385@qq.com>
2018-06-26 22:44:30 +0800

    modified:   web/user/css/user.css 	modified:   web/user/js/user.js 	new file:   web/user/userX.html

--- web/user/js/user.js
+++ web/user/js/user.js
@@ -1,29 +1,50 @@
 //页面跳转
-var jump_classchoose = document.getElementById("CC");
-var jump_userinfo = document.getElementById("UI");
-var jump_userinfoedit = document.getElementById("edit-info");
+var jump_profile_edit = document.getElementById("btn-profile-edit");
+var jump_classes_edit = document.getElementById("btn-classes-edit");
+var jump_classes_choose = document.getElementById("main-bar-2");
+var jump_profile_display = document.getElementById("main-bar-1");
 
-var classes_choose = document.getElementById("classes-choose");
-var user_information = document.getElementById("user-information");
-var user_information_edit = document.getElementById("user-information-edit");
+var class_choose_area = document.getElementById("class-choose-area");
+var profile_display_area = document.getElementById("profile-display-area");
+var profile_edit_area = document.getElementById("profile-edit-area");
+var classes_edit_area = document.getElementById("classes-edit-area");
 
-jump_classchoose.onclick = function(){
-    jump_classchoose.style.color='#B2DFEE';
-    jump_userinfo.style.color='#FFF';
-    classes_choose.style.display = 'block';
-    user_information.style.display = 'none';
-    user_information_edit.style.display = 'none';
+jump_profile_edit.onclick = function(){
+    jump_classes_choose.style.color = "black";
+    jump_profile_display.style.color = 'black';
+
+    profile_edit_area.style.display = "block";
+    class_choose_area.style.display ="none";
+    profile_display_area.style.display ="none";
+    classes_edit_area.style.display ="none";
 }
-jump_userinfo.onclick = function(){
-    jump_classchoose.style.color='#FFF';
-    jump_userinfo.style.color='#B2DFEE';
-    classes_choose.style.display = 'none';
-    user_information.style.display = 'block';
-    user_information_edit.style.display = 'none';
+
+jump_classes_edit.onclick = function(){
+    jump_classes_choose.style.color = "black";
+    jump_profile_display.style.color = 'black';
+
+    profile_edit_area.style.display = "none";
+    class_choose_area.style.display ="none";
+    profile_display_area.style.display ="none";
+    classes_edit_area.style.display ="block";
 }
-jump_userinfoedit.onclick = function(){
-    classes_choose.style.display = 'none';
-    user_information.style.display = 'none';
-    user_information_edit.style.display = 'block';
+
+jump_classes_choose.onclick = function(){
+    jump_classes_choose.style.color = "#428bca";
+    jump_profile_display.style.color = 'black';
+
+    profile_edit_area.style.display = "none";
+    class_choose_area.style.display ="block";
+    profile_display_area.style.display ="none";
+    classes_edit_area.style.display ="none";
 }
-        
+
+jump_profile_display.onclick = function(){
+    jump_classes_choose.style.color = "black";
+    jump_profile_display.style.color = '#428bca';
+
+    profile_edit_area.style.display = "none";
+    class_choose_area.style.display ="none";
+    profile_display_area.style.display ="block";
+    classes_edit_area.style.display ="none";
+}
